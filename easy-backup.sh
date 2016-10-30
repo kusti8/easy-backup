b96cd55af79cef5c0f2a58a57207350ca989ed9f
kusti8 <kusti8@gmail.com>
2016-10-30 16:51:24 -0400

    Backup files, not folder

--- easy-backup.sh
+++ easy-backup.sh
@@ -23,7 +23,7 @@ fi
 
 OUTPUT=$1
 
-tar --one-file-system -czpf "$1/boot.tar.gz" /boot
+tar --one-file-system -czpf "$1/boot.tar.gz" /boot/*
 tar --one-file-system -czpf "$1/root.tar.gz" /
 
 echo "BACKED UP!!!"
