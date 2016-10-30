c0d5e293c962c3a68acb0a2c9cb26fbc23724797
kusti8 <kusti8@gmail.com>
2016-10-30 16:22:30 -0400

    Add error checking and fix errors

--- easy-backup.sh
+++ easy-backup.sh
@@ -1,6 +1,28 @@
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ $# -eq 0 ]
+  then echo "No arguments supplied"
+  exit
+fi
+
+if [ ! -d "$1" ]
+  then echo "Directory does not exist"
+  exit
+fi
+
+if [ -z "$(mount | grep "$1")" ]
+  then echo "Not a mount point"
+  exit
+fi
+
 OUTPUT=$1
 
-tar --one-file-system --czpf "$1/boot.tar.gz" /boot
+tar --one-file-system -czpf "$1/boot.tar.gz" /boot
 tar --one-file-system -czpf "$1/root.tar.gz" /
 
 echo "BACKED UP!!!"
